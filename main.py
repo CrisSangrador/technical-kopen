a373c4232d7cd1de7d86fb40ef9389ebdbdef260
CrisSangrador <cris.sangrador@gmail.com>
2024-01-18 15:38:54 +0100

    added inputs so user can check their own amounts

--- main.py
+++ main.py
@@ -12,13 +12,18 @@ name_csv = "mortgage.csv"
 
 calculate_mortage(due, interest, monthly_payment, yearly_extra_payment, name_csv) # We call the function
 
+reply = input("Now, do you want to define your own parameters? ")
 
-"""print("Now, define your own parameters!")
+if reply.lower() == "yes":
 
-due2 = float(input("Introduce your total amount due: "))
-interest2 = float(input("Introduce your monthly interest: ")) / 100
-monthly_payment2 = float(input("Introduce your monthly payment: "))
-yearly_extra_payment2 = float(input("Introduce your yearly extra payment: "))
+    due2 = float(input("Introduce your total amount due: "))
+    interest2 = float(input("Introduce your monthly interest: ")) / 100
+    monthly_payment2 = float(input("Introduce your monthly payment: "))
+    yearly_extra_payment2 = float(input("Introduce your yearly extra payment: "))
+    name_csv2 = input("Introduce the name you want to give to your CSV file: ")
+    
+    calculate_mortage(due2, interest2, monthly_payment2, yearly_extra_payment2, name_csv2)
 
-calculate_mortage(due2, interest2, monthly_payment2, yearly_extra_payment2)"""
 
+else: 
+    pass
