71091fc8d88c23ae30e5e739590442003082423b
CrisSangrador <cris.sangrador@gmail.com>
2024-01-18 14:39:42 +0100

    updated to include the csv

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ interest = 0.5 / 100 # Variable with the monthly interest divided by 100 to calc
 monthly_payment = 375 # Variable with the monthly payment
 yearly_extra_payment = 500 # Variable with the extra payment that is made every 12 months 
 
+name_csv = "mortgage.csv"
 
-calculate_mortage(due, interest, monthly_payment, yearly_extra_payment) # We call the function
+
+calculate_mortage(due, interest, monthly_payment, yearly_extra_payment, name_csv) # We call the function
 
 
-print("Now, define your own parameters!")
+"""print("Now, define your own parameters!")
 
 due2 = float(input("Introduce your total amount due: "))
 interest2 = float(input("Introduce your monthly interest: ")) / 100
 monthly_payment2 = float(input("Introduce your monthly payment: "))
 yearly_extra_payment2 = float(input("Introduce your yearly extra payment: "))
 
-calculate_mortage(due2, interest2, monthly_payment2, yearly_extra_payment2)
+calculate_mortage(due2, interest2, monthly_payment2, yearly_extra_payment2)"""
 
